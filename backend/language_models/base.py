b89da0a318fb8901abcf6901ed840c293fe8ff44
shahmeer99 <mshahmeer99@gmail..com>
2024-08-15 15:48:57 +0300

    additional error handling to base.py extract_value_citation

--- backend/language_models/base.py
+++ backend/language_models/base.py
@@ -21,34 +21,51 @@ class LanguageModel(ABC):
         try:
             return eval(s)
         except:
-            s = s.replace("{", "").replace("}", "").replace("'", "").replace('"', "")
-            ret = {}
-            key_val_pairs = s.split(",")
-            for pair in key_val_pairs:
-                key, val = pair.split(":")
-                ret[key.strip()] = val.strip()
-            return ret
+            try:
+                s = s.replace("{", "").replace("}", "").replace("'", "").replace('"', "")
+                ret = {}
+                key_val_pairs = s.split(",")
+                for pair in key_val_pairs:
+                    print("PAIR: ", pair)
+                    key, val = pair.split(":")
+                    ret[key.strip()] = val.strip()
+                return ret
+            except Exception as e:
+                print("ERROR: ", str(e))
+                return None
         
     def extract_value_citation(self, model_response: str, retrieved: list) -> str:
         # Parse model output and return
 
         # Method 1: Expected JSON format, no noise
         try:
-            # print("*"*50)
             # print("METHOD 1 MODEL RESPONSE: ", model_response)
             response_dict = {k:str(v) for k,v in eval(model_response).items()}
             # print("RESPONSE DICT: ", response_dict)
-            value = response_dict["value"]
+            value = response_dict["value"] if response_dict["value"].lower().strip() not in ["", "none", "unknown", 'n/a', "':"] else None
             # print("VALUE: ", value)
-            table_name = response_dict["table_name"] if response_dict["table_name"].lower().strip() not in ["", "none", "unknown"] else None
-            # print("TABLE NAME: ", table_name)
-            row_number = response_dict["row_number"] if str(response_dict["row_number"]).lower().strip() not in ["", "none", "unknown"] else None
-            # print("ROW NUMBER: ", row_number)
-            # print("RETRIEVED: ", retrieved)
-            retrived_object = retrieved[int(response_dict["object_id"].split(" ")[-1])]["values"] if response_dict["object_id"] not in ["", "none", "unknown"] else None
-            # print("RETRIVED OBJECT: ", retrived_object)
-            retrived_object = self.stringified_dict_to_dict(retrived_object) if retrived_object != None and type(retrived_object) == str else retrived_object
+            if value != None:
+                table_name = response_dict["table_name"] if response_dict["table_name"].lower().strip() not in ["", "none", "unknown", 'n/a'] else None
+                # print("TABLE NAME: ", table_name)
+                row_number = response_dict["row_number"] if str(response_dict["row_number"]).lower().strip() not in ["", "none", "unknown", ' n/a'] else None
+                # print("ROW NUMBER: ", row_number)
+                # print("RETRIEVED: ", retrieved)
+                retrived_object = retrieved[int(response_dict["object_id"].split(" ")[-1])]["values"] if response_dict["object_id"] not in ["", "none", "unknown", 'n/a'] else None
+                # print("RETRIVED OBJECT: ", retrived_object)
+                print("RETRIEVED OBJECT 1:" , retrived_object)
+                retrived_object = self.stringified_dict_to_dict(retrived_object) if retrived_object != None and type(retrived_object) == str else retrived_object
+                print("RETRIEVED OBJECT 2", retrived_object)
+            else:
+                table_name = None
+                row_number = None
+                retrived_object = None
 
+            print("RETURNING: ", {
+                "value": value,
+                "table_name": table_name,
+                "row_number": row_number,
+                "tuple" : retrived_object
+            })
 
             return {
                 "value": value,
@@ -66,11 +83,17 @@ class LanguageModel(ABC):
             start = model_response.find("{")
             end = model_response.rfind("}") + 1
             response_dict = eval(model_response[start:end])
-            value = response_dict["value"]
-            table_name = response_dict["table_name"] if response_dict["table_name"].lower().strip() not in ["", "none", "unknown"] else None
-            row_number = response_dict["row_number"] if str(response_dict["row_number"]).lower().strip() not in ["", "none", "unknown"] else None
-            retrived_object = retrieved[int(response_dict["object_id"].split(" ")[-1])]["values"] if response_dict["object_id"] not in ["", "none", "unknown"] else None
-            retrived_object = self.stringified_dict_to_dict(retrived_object) if retrived_object != None and type(retrived_object) == str else retrived_object
+            value = response_dict["value"] if response_dict["value"].lower().strip() not in ["", "none", "unknown", 'n/a', "':"] else None
+            if value != None:
+                table_name = response_dict["table_name"] if response_dict["table_name"].lower().strip() not in ["", "none", "unknown"] else None
+                row_number = response_dict["row_number"] if str(response_dict["row_number"]).lower().strip() not in ["", "none", "unknown"] else None
+                retrived_object = retrieved[int(response_dict["object_id"].split(" ")[-1])]["values"] if response_dict["object_id"] not in ["", "none", "unknown"] else None
+                retrived_object = self.stringified_dict_to_dict(retrived_object) if retrived_object != None and type(retrived_object) == str else retrived_object
+            else:
+                table_name = None
+                row_number = None
+                retrived_object = None
+
             return {
                 "value": value,
                 "table_name": table_name,
@@ -88,40 +111,42 @@ class LanguageModel(ABC):
                 value_start = model_response.find("value : ") + len("value : ")
                 value_end = model_response.find(" ", value_start)
                 value = model_response[value_start:value_end]
-                value = value if value.lower().strip() not in ["", "none", "unknown"] else None
+                value = value if value.lower().strip() not in ["", "none", "unknown", "':"] else None
+
+                # Get citation
+                try:
+                    # find the term "table_name : " in the response and extract everything after that till the next white space
+                    table_name_start = model_response.find("table_name : ") + len("table_name : ")
+                    table_name_end = model_response.find(" ", table_name_start)
+                    table_name = model_response[table_name_start:table_name_end]
+                    table_name = table_name if table_name.lower().strip() not in ["", "none", "unknown"] else None
+                except:
+                    table_name = None
+
+                try:
+                    # find the term "row_number : " in the response and extract everything after that till the next white space
+                    row_number_start = model_response.find("row_number : ") + len("row_number : ")
+                    row_number_end = model_response.find(" ", row_number_start)
+                    row_number = model_response[row_number_start:row_number_end]
+                    row_number = row_number if row_number.lower().strip() not in ["", "none", "unknown"] else None
+                except:
+                    row_number = None
+
+                try:
+                    # find the term "object_id : " in the response and extract everything after that till the next white space
+                    object_id_start = model_response.find("object_id : ") + len("object_id : ")
+                    object_id_end = model_response.find(" ", object_id_start)
+                    object_id = model_response[object_id_start:object_id_end]
+                    retrived_object_index = int(object_id.split(" ")[-1]) if object_id not in ["", "none", "unknown"] else None
+                    retrived_object = retrieved[retrived_object_index]["values"] if retrived_object_index != None else None
+                except:
+                    retrived_object = None
 
             except:
                 value = None
-
-            # Get citation
-            try:
-                # find the term "table_name : " in the response and extract everything after that till the next white space
-                table_name_start = model_response.find("table_name : ") + len("table_name : ")
-                table_name_end = model_response.find(" ", table_name_start)
-                table_name = model_response[table_name_start:table_name_end]
-                table_name = table_name if table_name.lower().strip() not in ["", "none", "unknown"] else None
-            except:
                 table_name = None
-
-            try:
-                # find the term "row_number : " in the response and extract everything after that till the next white space
-                row_number_start = model_response.find("row_number : ") + len("row_number : ")
-                row_number_end = model_response.find(" ", row_number_start)
-                row_number = model_response[row_number_start:row_number_end]
-                row_number = row_number if row_number.lower().strip() not in ["", "none", "unknown"] else None
-            except:
                 row_number = None
-
-            try:
-                # find the term "object_id : " in the response and extract everything after that till the next white space
-                object_id_start = model_response.find("object_id : ") + len("object_id : ")
-                object_id_end = model_response.find(" ", object_id_start)
-                object_id = model_response[object_id_start:object_id_end]
-                retrived_object_index = int(object_id.split(" ")[-1]) if object_id not in ["", "none", "unknown"] else None
-                retrived_object = retrieved[retrived_object_index]["values"] if retrived_object_index != None else None
-            except:
                 retrived_object = None
-                
 
             return {"value": value, "table_name": table_name, "row_number": row_number, "tuple" : retrived_object}
 
